1b9681ec293882cdf02069523fc713eb62521f42
mohammad2h3 <88118210+mohammad2h3@users.noreply.github.com>
2021-07-29 02:02:13 +0200

    Update algebra.sh

--- algebra.sh
+++ algebra.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 counter=0
 while true; do
-  if /home/hadoop/latest/bin/hadoop fs -ls /apps/hdtech/bds/quality-rt/dt=$DATE_YEST_FORMAT2 then
-       exit 0
-  else
        counter=$((counter+1))
        echo "Tensorflow Packet created $counter time(s); / ??????? " 
        sleep 1800
-  fi
 done
